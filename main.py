10ec219d33754d78f9f3d713ddfcc314c3607ec3
Gunjan16 <37958425+Gunjan16@users.noreply.github.com>
2023-02-23 12:30:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello new commit!"
+    return "Hello new commit - changes!"
 # run the app.
 if __name__ == "__main__":
     app.run()
