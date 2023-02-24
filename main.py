bd3c92dea156ac75387678d7459c4e8e80e6c217
Gunjan16 <37958425+Gunjan16@users.noreply.github.com>
2023-02-24 15:06:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello Jenkins!"
+    return "Hello All!"
 # run the app.
 if __name__ == "__main__":
     app.run()
