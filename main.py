e6fbedafcdd97f9bbdafb02f53c6b84bab12f0b2
Gunjan16 <37958425+Gunjan16@users.noreply.github.com>
2023-02-24 16:02:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Jenkins Pipeline!"
+    return "New Pipeline !"
 # run the app.
 if __name__ == "__main__":
     app.run()
